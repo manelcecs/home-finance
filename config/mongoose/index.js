e06844abd88ce9e45077bda7557077aa42025817
Manuel C Pérez <manuelc.p.g@gmail.com>
2020-10-25 12:00:09 +0100

    Add security params

--- config/mongoose/index.js
+++ config/mongoose/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const mongose = require('mongoose');
-let dbUriRead= Buffer.from('bW9uZ29kYitzcnY6Ly9hZG1pbjphZG1pbkBzYW5kYm94LWNsdXN0ZXIuZHZvdXkubW9uZ29kYi5uZXQvaG9tZS1maW5hbmNlP3JldHJ5V3JpdGVzPXRydWUmdz1tYWpvcml0eQ==', 'base64').toString();
+const dbURI = require('../security/index').dbURI;
+let dbUriRead= Buffer.from(dbURI, 'base64').toString();
 
 mongose.connect(dbUriRead, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true });
 
@@ -9,10 +10,8 @@ mongose.Promise = global.Promise;
 //Load models
 require('../../model/user.model');
 
-
 let db = mongose.connection;
 console.log("Models:", db.models);
-
 
 db.on('error', console.error.bind(console, 'MongoDB connection error: '));
 
