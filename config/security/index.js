aaab2b2b2db920515019163ba142eec79f42ae2f
Manuel C Pérez <manuelc.p.g@gmail.com>
2020-11-01 19:40:41 +0100

    add: full coverage to userService
    Added test enviroment
    Added test userService

--- config/security/index.js
+++ config/security/index.js
@@ -1,7 +1,14 @@
 const dbURI ='bW9uZ29kYitzcnY6Ly9hZG1pbjphZG1pbkBzYW5kYm94LWNsdXN0ZXIuZHZvdXkubW9uZ29kYi5uZXQvaG9tZS1maW5hbmNlP3JldHJ5V3JpdGVzPXRydWUmdz1tYWpvcml0eQ==';
+const dbURItest = 'bW9uZ29kYitzcnY6Ly9hZG1pbjphZG1pbkBzYW5kYm94LWNsdXN0ZXIuZHZvdXkubW9uZ29kYi5uZXQvaG9tZS1maW5hbmNlLXRlc3Q_cmV0cnlXcml0ZXM9dHJ1ZSZ3PW1ham9yaXR5==';
 const JWTkey = 'primary@$ecurty';
 const JWTcad = '48h';
 
+const profile = 'dev';
+
+process.env.profile = profile;
+
 process.env.dbURI = dbURI;
+process.env.dbURItest = dbURItest;
 process.env.JWTkey = JWTkey;
 process.env.JWTcad = JWTcad;
+
